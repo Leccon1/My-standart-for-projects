a833caf26430ed5af5b9bc73102c8fa0b6fce8f1
Leccon1 <dlavania703@gmail.com>
2025-08-10 23:47:48 +0300

    add new files

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  base: 'URL REPOZITORY',
+	base: 'URL REPOZITORY',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
+			'@styles': path.resolve(__dirname, './src/style/sass'),
+			'@js': path.resolve(__dirname, './src/js'),
+			'@images': path.resolve(__dirname, './src/assets/images'),
+			'@fonts': path.resolve(__dirname, './src/assets/fonts'),
 		},
 	},
 })
