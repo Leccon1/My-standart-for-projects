ba60ce62b496114215091084d49f8a76c496a81b
Leccon1 <dlavania703@gamil.com>
2025-11-03 00:01:40 +0300

    feat(update): add react

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,27 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
 
 export default defineConfig({
-	base: 'URL REPO',
+  base: 'URL REPO',
 
-	plugins: [
-		checker({
-			stylelint: {
-				lintCommand: 'stylelint "./src/**/*.scss"',
-			},
-		}),
-	],
+  plugins: [
+    [react()],
+    checker({
+      stylelint: {
+        lintCommand: 'stylelint "./src/**/*.scss"',
+      },
+    }),
+  ],
 
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			'@styles': path.resolve(__dirname, './src/style/sass'),
-			'@js': path.resolve(__dirname, './src/js'),
-			'@images': path.resolve(__dirname, './src/assets/images'),
-			'@fonts': path.resolve(__dirname, './src/assets/fonts'),
-		},
-	},
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/style/sass'),
+      '@js': path.resolve(__dirname, './src/js'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
+      '@fonts': path.resolve(__dirname, './src/assets/fonts'),
+    },
+  },
 })
