78d8077f08a64ee802e7eaa046e7f3fd2cf532e3
Leccon1 <dlavania703@gamil.com>
2025-12-23 19:56:48 +0300

    feat: update alias

--- vite.config.js
+++ vite.config.js
@@ -25,13 +25,32 @@ export default defineConfig({
   assetsInclude: ['**/*.svg'],
   resolve: {
     alias: {
+      // base
       '@': path.resolve(__dirname, './src'),
-      '@styles': path.resolve(__dirname, './src/style/'),
+
+      // assets
+      '@assets': path.resolve(__dirname, './src/assets'),
       '@images': path.resolve(__dirname, './src/assets/images'),
       '@icons': path.resolve(__dirname, './src/assets/icons'),
       '@fonts': path.resolve(__dirname, './src/assets/fonts'),
-      '@data': path.resolve(__dirname, './src/data'),
+      '@favicons': path.resolve(__dirname, './src/assets/favicons'),
+
+      // styles
+      '@styles': path.resolve(__dirname, './src/style'),
+      '@style-base': path.resolve(__dirname, './src/style/base'),
+      '@style-helpers': path.resolve(__dirname, './src/style/helpers'),
+      '@style-setting': path.resolve(__dirname, './src/style/setting'),
+
+      // components
       '@components': path.resolve(__dirname, './src/components'),
+      '@common': path.resolve(__dirname, './src/components/common'),
+      '@layout': path.resolve(__dirname, './src/components/layout'),
+      '@pages': path.resolve(__dirname, './src/components/pages'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
+
+      // logic
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
 })
